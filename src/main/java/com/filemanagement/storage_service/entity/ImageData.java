45a97b62274c3c787afd6d90fb0289ec09bae5b2
sreelatha <sreelatha.reddemmagari@infoservices.com>
2025-04-17 19:42:00 +0530

    upload and download images

---
+++ src/main/java/com/filemanagement/storage_service/entity/ImageData.java
@@ -0,0 +1,24 @@
+package com.filemanagement.storage_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ImageData")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "imageData",length = 1000)
+    private byte[] imageData;
+}
---
+++ src/main/java/com/filemanagement/storage_service/service/StorageService.java
@@ -0,0 +1,33 @@
+package com.filemanagement.storage_service.service;
+
+import com.filemanagement.storage_service.Repository.StorageRepository;
+import com.filemanagement.storage_service.entity.ImageData;
+import com.filemanagement.storage_service.util.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class StorageService {
+    @Autowired
+    private StorageRepository storageRepository;
+    public String uploadImage(MultipartFile file) throws IOException {
+
+        ImageData imageData = storageRepository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtils.compressImage(file.getBytes())).build());
+        if (imageData != null) {
+            return "file uploaded successfully : " + file.getOriginalFilename();
+        }
+        return null;
+    }
+    public byte[] downloadImage(String fileName){
+        Optional<ImageData> dbImageData = storageRepository.findByName(fileName);
+        byte[] images=ImageUtils.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
+}
